eeaacdef2db9234a57dfdb64c7f9dd66455b7fa8
Shweta Srivastava <81436968+Shweta2254@users.noreply.github.com>
2021-05-25 14:01:06 +0530

    card effects

--- javascript.js
+++ javascript.js
@@ -1,5 +1,53 @@
-function myFunction() {
-    var x = document.getElementById("ima");
+function myFunction1() {
+    var x = document.getElementById("im1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction2() {
+    var x = document.getElementById("im2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction3() {
+    var x = document.getElementById("im3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction4() {
+    var x = document.getElementById("im4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction5() {
+    var x = document.getElementById("im5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction6() {
+    var x = document.getElementById("im6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+function myFunction7() {
+    var x = document.getElementById("im7");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
